652f9a76bf35d62f740f353677b06192c333fdc8
hkamori <hk1337@proton.me>
2025-01-25 12:31:33 +0100

    Create viewerscount.js

---
+++ assets/js/viewerscount.js
@@ -0,0 +1,13 @@
+let viewerCount = localStorage.getItem("viewerCount");
+
+if (!viewerCount) {
+  viewerCount = 0;
+}
+
+viewerCount++;
+localStorage.setItem("viewerCount", viewerCount);
+document.getElementById("visitor-count").innerText = `This page has been visited ${viewerCount} times!`;
+function checkViewers() {
+  console.log(`This page has been visited ${localStorage.getItem("viewerCount")} times.`);
+}
+window.checkViewers = checkViewers;
